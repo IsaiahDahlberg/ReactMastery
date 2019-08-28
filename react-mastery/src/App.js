fbfcabdb197a147f8df96c633ba4de329a5829e0
IsaiahDahlberg <IsaiahD@magenic.com>
2019-08-28 15:30:34 -0500

    Add minor style changes

--- react-mastery/src/App.js
+++ react-mastery/src/App.js
@@ -6,21 +6,22 @@ import './App.css';
 
 function App() {
   var mockSM = [
-    {index: 9, title: "Blah blah"},
-    {index: 8, title: "Blah"},
-    {index: 7, title: "Golden Anniversary"},
-    {index: 6, title: "Pomp and Circumstnace"},
-    {index: 5, title: "Golum's Theme"},
-    {index: 4, title: "Riders of Rohan"},
-    {index: 3, title: "You got a Friend in me"},
+    {index: 1, title: "Fur Elise"},
     {index: 2, title: "Ashkon Farewell"},
-    {index: 1, title: "Fur Elise"}
+    {index: 3, title: "You got a Friend in me"},
+    {index: 4, title: "Riders of Rohan"},
+    {index: 5, title: "Golum's Theme"},
+    {index: 6, title: "Pomp and Circumstnace"},
+    {index: 7, title: "Golden Anniversary"},
+    {index: 8, title: "Blah"},
+    {index: 9, title: "Blah blah"}
   ]
 
   return (
     <div className="base">
       <header>
         <h1 className="webSiteHeader">Sheet Music</h1>
+        <button>Add</button>
         <SheetMusicQueryPanel sheetMusic={mockSM} />
       </header>
     </div>
---
+++ react-mastery/src/Components/AddSheetMusic/AddSheetMusicPopup.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../Styles/AddSheetMusicPopup.css"
+function AddSheetMusicPopup (props) {
+    return(
+        <div className="AddSheetMusicPopup">
+            <h1>AddSheetMusicPopup</h1>
+        </div>
+    )
+}
+
+export default AddSheetMusicPopup.js
